e07df1c7b80ed1439159d45bec4c205d6efad269
jovanhan2 <jovanhan2@gmail.com>
2019-02-04 18:12:50 +0000

    updated with messages

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+  socket.on('new message', function(msg){
     console.log('msg recieved: ' + msg);
-    io.emit('chat message', msg);
+    io.emit('new message', msg);
   });
 });
 io.on('connection', function(socket){
