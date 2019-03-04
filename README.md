119f6d0a3605bd7dc26e0ea99860c02193b88f62
jovanhan2 <jovanhan2@gmail.com>
2019-03-04 13:49:47 +0000

    read write with better-sqlite3

--- README.md
+++ README.md
@@ -18,3 +18,18 @@ column -s, -t < resources/questions.csv | less -#2 -N -S
   }
 }
 ```
+
+# Installation issues
+I was trying to install better-sqlite3 but kept getting errors about not finding VCBuild and MSBuild.
+
+I initally tried npm install -g node-gyp by itself but then got different errors.
+
+I then tried to use npm install -g windows-build-tools which installed Visual Studio 2017 and did NOT include the v140 toolset and was getting and error about this.
+
+But Visual Studio 2015 does includes the v140 toolset...
+You can force windows-build-tools to install VS2015 instead of VS2017 with this:
+npm install --vs2015 -g windows-build-tools
+
+better-sqlite3 installed fine after this.
+
+https://github.com/nodejs/node-gyp/issues/1486
--- src/sqlManager.js
+++ src/sqlManager.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 
-var sqlite3 = require('sqlite3').verbose();
+var Database = require('better-sqlite3');
 
 
 class sqlManager {
@@ -14,11 +14,11 @@ class sqlManager {
             var headers = self.setHeader(type)
             console.log("Headers " + headers)
             createTable = "CREATE TABLE " + type + " (";
-            for (var i = 0; i< headers.length; i++) {
+            for (var i = 0; i < headers.length; i++) {
                 createTable = createTable.concat(headers[i] + " string,")
             }
             // get rid of trailing commar
-            createTable = createTable.slice(0, -1); 
+            createTable = createTable.slice(0, -1);
             createTable = createTable.concat(");")
             // console.log(createTable)
             self.db.run(createTable);
@@ -33,21 +33,7 @@ class sqlManager {
         this.type = type
         // storage
         // test
-        this.db = new sqlite3.Database("resources/" + filename + ".db");
-    }
-
-
-    checkLogin(Username, Password, data) {
-        for (var i = 0; i < data.length; i++) {
-            if (Username == data[i].Username && Password == data[i].Password) {
-                return { state: "Success", name: data[i].Name, caregiver: data[i].Caregiver }
-            }
-        }
-        return { state: "Fail", name: "", caregiver: "" }
-        // var result = data.map(item => (item.Username == Username && item.Password == Password) ? true : false)
-        // console.log(result)
-        // if (result.includes(true)) return true
-        // else return false;
+        this.db = new Database("resources/" + filename + ".db", { verbose: console.log });
     }
     filterDataFromName(data, name) {
         var list = []
@@ -59,19 +45,25 @@ class sqlManager {
         }
         return list
     }
-
-    async readCheckLogin(data) {
+    // readDatabase(callback) {
+    //     this.db.all("SELECT * from userDetails;", function (err, all) {
+    //         // console.log(all)
+    //         callback(err, all);
+    //     });
+    // };
+    readCheckLogin(data) {
         console.log("Recieved login data: " + data.Username + ' | Password ' + data.Password + " |Name: ")
-        // console.log("Reading data from " + 'resources/' + this.type + '.csv')
-        this.db
-        csv()
-            .fromFile(this.Path)
-            .then((jsonObj) => { })
-        // Async / await usage
-        const jsonArray = await csv().fromFile(this.Path);
-        // console.log(jsonArray)
-        // console.log(this.checkLogin("user","pass",jsonArray))
-        return this.checkLogin(data.Username, data.Password, jsonArray)
+        const stmt = this.db.prepare('SELECT * FROM userDetails WHERE Username=? AND Password=?;');
+        const result = stmt.all('user', 'password');
+        if (result.length == 0) {
+            return { state: "Fail", name: "", caregiver: "" }
+        }
+        else if (result.length == 1) {
+            return { state: "Success", name: result[0].Name, caregiver: result[0].Caregiver }
+        }
+        else {
+            console.log("Multiple similar entries")
+        }
     }
     async readMedicationData(data) {
         console.log("Reading medication data for user " + data)
@@ -92,32 +84,32 @@ class sqlManager {
 
 
     write(data) {
-        var self = this
-        this.db.serialize(function () {
-     
-            var insertTable = "INSERT INTO " + self.type + " (";
-            // setup columns
-            Object.keys(data).forEach(function(key) {
-                insertTable = insertTable.concat(key + ",")
-            });
-            insertTable = insertTable.slice(0, -1); 
-            insertTable = insertTable.concat(") VALUES (")
-            // add values
-            Object.keys(data).forEach(function(key) {
-                insertTable = insertTable.concat("'" + data[key] + "'" + ",")
 
-            });
-            insertTable = insertTable.slice(0, -1); 
-            insertTable = insertTable.concat(");")
-            // console.log(createTable)
-            self.db.run(insertTable);
-            console.log("Inserted into table with query " + insertTable);
-            // var stmt = self.db.prepare(insertTable);
-            // for (var i = 0; i < 10; i++) {
-            //     stmt.run("Ipsum " + i);
-            // }
-            // stmt.finalize();
+
+        var insertTable = "INSERT INTO " + this.type + " (";
+        // setup columns
+        Object.keys(data).forEach(function (key) {
+            insertTable = insertTable.concat(key + ",")
         });
+        insertTable = insertTable.slice(0, -1);
+        insertTable = insertTable.concat(") VALUES (")
+        // add values
+        Object.keys(data).forEach(function (key) {
+            insertTable = insertTable.concat("'" + data[key] + "'" + ",")
+
+        });
+        insertTable = insertTable.slice(0, -1);
+        insertTable = insertTable.concat(");")
+        // console.log(createTable)
+        const stmt = this.db.prepare(insertTable);
+        const info = stmt.run();
+        console.log("Inserted into table with query " + insertTable + " info :" + JSON.stringify(info));
+        // var stmt = self.db.prepare(insertTable);
+        // for (var i = 0; i < 10; i++) {
+        //     stmt.run("Ipsum " + i);
+        // }
+        // stmt.finalize();
+
         this.db.close()
 
 
@@ -199,15 +191,23 @@ class sqlManager {
 
 }
 // sqlManager = new sqlManager("questions", "test")
+// var res = sqlManager.readCheckLogin({Username: 'user', Password: 'password'})
+
+// console.log(JSON.stringify(res))
+// sqlManager = new sqlManager("questions", "test")
 // var tables = ['addMedication','userDetails','timeTaken','questions','watchInfo' ]
 // for (var i = 0 ; i < tables.length; i ++) {
 //     sqlManager.createTable(tables[i])
 // }
 // sqlManager.db.close();
-var tableName = "userDetails"
-sqlManager = new sqlManager(tableName,"test")
-const records = { Username: 'user', Password: 'pass', Name: "name", Caregiver: "careGiver" }
-sqlManager.write(records)
-// user reg
 
+// sqlManager = new sqlManager("userDetails", "test")
+// const records = { Username: 'user2', Password: 'pass2', Name: "name2", Caregiver: "careGiver2" }
+// sqlManager.write(records)
+
+
+// const records = { User: 'user', Name: 'medicationName2',Instruction: 'Instruction', Mon: false, Tue: true, Wed: false, 
+// Thu: false, Fri: true, Sat:true, Sun: false, Morning :true, Afternoon : true, Night:false, Meal: true }
+// sqlManager = new sqlManager("addMedication","test")
+// sqlManager.write(records)
 module.exports = sqlManager;
